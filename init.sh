a380303b433525670c91d8860075c25c9f2398b6
Daisuke Takahashi <d.takahashi1811@gmail.com>
2017-01-21 16:37:03 +0900

    revised init.sh script

--- init.sh
+++ init.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(cd $(dirname "$0"); pwd)"
+
 echo "Creating soft links for dotfiles"
 
-for f in .??*
+for f in `ls -a|grep '^\.[a-zA-Z][a-zA-Z]*'`
 do 
   [[ "$f" == .git* ]] && continue
   [[ "$f" == ".DS_Config" ]] && continue
   if [ ! -e "$HOME/$f" ]; then 
     echo "creating $f" && ln -s "$f" "$HOME/$f"
+    # echo "creating $f" 
   else
     echo "$HOME/$f already exists."
   fi
