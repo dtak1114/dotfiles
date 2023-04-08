97cc3d94d946f7e5ff61fb0a5c0e36a763c1eff8
Daisuke Takahashi <daisuke.takahashi@sony.com>
2023-04-08 17:05:35 +0900

    add: script

---
+++ scripts/chatgpt/chatgpt.py
@@ -0,0 +1,54 @@
+import openai
+import os
+
+# Set up OpenAI API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+CHARACTER = 'You are a general helper CLI'
+
+# Define model and other parameters
+model = "gpt-3.5-turbo"
+temperature = 0
+#  max_tokens = 3000
+
+# Define function to generate response
+def generate_response(messages):
+    # Call the API to generate the response
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+    )
+    # Extract the text from the response and return it
+    return response.choices[0].message.content
+
+def generate_message(role, content):
+    return {
+            'role': role,
+            'content': content
+    }
+
+def initialize_messages():
+    messages = []
+    messages.append(generate_message('system',CHARACTER))
+    return messages
+
+
+def main():
+    messages = initialize_messages()
+
+    # Start the conversation loop
+    while True:
+        # Get user input
+        user_input = input("You >> ")
+        # Add user input to the prompt
+        messages.append(generate_message('user', user_input))
+        # Generate response
+        response = generate_response(messages)
+        print(response)
+        # print response
+        print("AI >> " + response + "\n")
+        # append to the messages
+        messages.append(generate_message('assistant', response))
+if __name__ == '__main__':
+    main()
