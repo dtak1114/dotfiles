3665f926e6fce10246df8449be409e1dd4f1be37
Daisuke Takahashi <daisuke.takahashi@sony.com>
2023-04-11 01:14:33 +0900

    add: logging the history

--- scripts/chatgpt/chatgpt.py
+++ scripts/chatgpt/chatgpt.py
@@ -2,11 +2,14 @@ import openai
 import os
 import time
 from functools import wraps
+import datetime
 
 # Set up OpenAI API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-CHARACTER = ''
+# write history to a file
+HISTORY_FILE = "./.gpt_history"
+HISTORY_LIMIT_BYTES = 1024 * 10
 
 # Define model and other parameters
 model = "gpt-3.5-turbo"
@@ -48,29 +51,46 @@ def generate_message(role, content):
             'content': content
     }
 
-def initialize_messages():
+def initialize_messages(role):
     messages = []
-    messages.append(generate_message('system',CHARACTER))
+    messages.append(generate_message('system',role))
     return messages
 
 
 def main():
-    messages = initialize_messages()
+    ROLE_TXT_PATH = os.path.join(os.path.dirname(__file__), 'role.txt')
+    with open(ROLE_TXT_PATH) as f:
+        role = f.read().strip()
+    messages = initialize_messages(role)
 
+    start = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    write_output_and_trim("===start session on {} ===\n".format(start))
     # Start the conversation loop
     while True:
         # Get user input
         user_input = input("You >> ")
+        write_output_and_trim("You >> " + user_input+ "\n")
         # Add user input to the prompt
         messages.append(generate_message('user', user_input))
         # Generate response
         response = generate_response(messages)
         # print response
         print("AI >> " + response + "\n")
+        write_output_and_trim("AI >> " + response+ "\n")
         # append to the messages
         messages.append(generate_message('assistant', response))
+
+def write_output_and_trim(output,filename=HISTORY_FILE,limit=HISTORY_LIMIT_BYTES):
+    if not os.path.exists(filename):
+        open('myfile.txt', 'w').close()
+    with open(filename, 'a') as f:
+        f.write(output)
+    if os.path.getsize(filename) > limit:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+        with open(filename, 'w') as f:
+            f.writelines(lines[1:])
+
 if __name__ == '__main__':
-    ROLE_TXT_PATH = os.path.join(os.path.dirname(__file__), 'role.txt')
-    with open(ROLE_TXT_PATH) as f:
-        CHARACTER = f.read().strip()
     main()
